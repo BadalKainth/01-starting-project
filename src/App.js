aba57e0ba0bbc309540d415069448888a9feed4a
badalkainth <badalkainth06@gmail.com>
2022-12-16 20:20:22 +0530

    Completed.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./component/Users/AddUser";
+import UsersList from "./component/Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => [
+      ...prevUsersList,
+      { name: uName, age: uAge, id: Math.random().toString() },
+    ]);
+  };
   return (
     <div>
-      <AddUser></AddUser>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
